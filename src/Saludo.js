79e7e9bb908f50026f36aafe1cfbc580694cbdb2
Fabio J. Novello <fjnovello@gmail.com>
2021-01-30 11:16:21 -0300

    Commit 1

---
+++ src/Saludo.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Saludo extends Component {
+    render() {
+        return (
+            <h1>Hola Mundo !</h1>
+        );
+    }
+
+}
+
+export default Saludo;
